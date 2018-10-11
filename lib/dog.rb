911fda0eedbd4f064063bf2578ffb13d6bce4144
student online <eliel.badon@gmail.com>
2018-10-11 20:25:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
   
-  def initialize(name, breed)
+  def initialise(name, breed)
     @name = name
     @breed = breed
   end
@@ -22,26 +22,3 @@ class Dog
   end
 end 
 
-
-class Dog
-  def initialise(name, breed)
-    @name = name
-    @breed = breed
-  end   
-  
-  def name
-    @name
-  end
-  
-  def name=(new_name)
-    @name = new_name
-  end
-  
-  def breed
-    @breed
-  end
-  
-  def breed=(new_breed)
-    @breed = new_breed
-  end 
-end 
