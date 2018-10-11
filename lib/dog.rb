8836d8deb8dfa75afbaf42acfd790300d23250c9
student online <eliel.badon@gmail.com>
2018-10-11 20:15:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,12 @@ class Dog
   def initialize(name)
     @name = name
   end
+  def name=(new_name)
+    @name = new_name 
   def name
     @name 
   end 
-  def name=(new_name)
-    @name = new_name 
+
   end 
 end 
 
