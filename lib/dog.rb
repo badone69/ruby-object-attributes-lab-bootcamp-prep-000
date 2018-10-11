6ffde4c28229c74e5d8c9d5329b00198932fdc74
student online <eliel.badon@gmail.com>
2018-10-11 20:24:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,16 +21,3 @@ class Dog
     @breed = new_breed
 end 
 
-class Person 
-  def initialize(name)
-    @name = name
-  end
-  def name 
-  end 
-  def name 
-  end 
-  def job 
-  end 
-  def job=
-  end 
-end 
